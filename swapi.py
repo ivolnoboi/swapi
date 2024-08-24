c02c79152fd604ea94d3e3f000ac39da2b7215df
Irina Volnoboi <irinavolnoboy@gmail.com>
2024-08-24 15:49:11 +0300

    Add descriptions of classes and functions

--- swapi.py
+++ swapi.py
@@ -1,8 +1,15 @@
+"""This module is used for sending requests, getting response from
+The Star Wars API and writing received information to a file.
+"""
+
+
 from pathlib import Path
 import requests
 
 
 class APIRequester:
+    """The class for sending requests to a server."""
+
     def __init__(self, base_url):
         self.base_url = base_url
 
@@ -17,13 +24,17 @@ class APIRequester:
 
 
 class SWRequester(APIRequester):
+    """The class for getting information from The Star Wars API."""
+
     def get_sw_categories(self):
+        """Get existing categories from The Star Wars API."""
         response = self.get('/')
         if response:
             return response.json().keys()
         return {}.keys()
 
     def get_sw_info(self, sw_type):
+        """Get information about a specific category."""
         response = self.get('/' + sw_type + '/')
         if response:
             return response.text
@@ -31,9 +42,12 @@ class SWRequester(APIRequester):
 
 
 def save_sw_data():
+    """Save information about all categories on The Star Wars to files."""
     Path('data').mkdir(exist_ok=True)
+
     swapi_requester = SWRequester('https://swapi.dev/api')
     categories = swapi_requester.get_sw_categories()
+
     for category in categories:
         info = swapi_requester.get_sw_info(category)
         with open(f'data/{category}.txt', 'w') as file:
