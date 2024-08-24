77da321f083ed7318739387a045759b71ef41d10
Irina Volnoboi <irinavolnoboy@gmail.com>
2024-08-24 13:54:13 +0300

    Create template

---
+++ swapi.py
@@ -0,0 +1,21 @@
+import requests
+
+
+class APIRequester:
+    def __init__(self, base_url):
+        pass
+
+    def get(self):
+        pass
+
+
+class SWRequester(APIRequester):
+    def get_sw_categories(self):
+        pass
+
+    def get_sw_info(self, sw_type):
+        pass
+
+
+def save_sw_data():
+    pass
